1eb9eee2167669e66ef80595435db11cb58805bb
chandan9074 <chandan15-11611@diu.edu.bd>
2022-03-09 17:34:45 +0600

    refact: update table

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,15 +2,17 @@ import { Table } from "antd";
 import React, { FC, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchStoryData } from '../../services/services';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid'
 import './homepage.css';
 
 const Homepage : React.FunctionComponent = () =>{
     const [story, setStory] = useState<any>([]);
     const [flag, setFlag] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(true)
     const navigate = useNavigate();
     let pageNo =  0;
     useEffect(() => {
-        if(story){
+        if(flag){
             fetchData();
             setFlag(false);
         }
@@ -23,43 +25,43 @@ const Homepage : React.FunctionComponent = () =>{
         const response = await fetchStoryData(pageNo);
         setStory((prevState:any) => [...prevState, ...response.hits]);
         pageNo = pageNo + 1;
+        setLoading(false)
     }
 
     const handleDataShow = (data : any) =>{
         navigate("/singledata", {state:{data:data}})
     }
 
-    const columns = [
+    const columns: GridColDef[] = [
         {
-            title: "Title",
-            dataIndex: "title"
+            field: 'objectID',
+            hide: true,
+            // identity: true
         },
+        { field: 'title', headerName: 'Title', width: 600 },
+        { field: 'author', headerName: 'Author', width: 200 },
+        { field: 'created_at', headerName: 'Created At', width: 250 },
         {
-            title: "Author",
-            dataIndex: "author"
-        },
-        {
-            title: "Created At",
-            dataIndex: "created_at"
-        },
-        {
-            title: "URL",
-            dataIndex: "url"
-        },
-    ]
+            field: 'url',
+            headerName: 'URL',
+            width: 350
+        }
+    ];
 
     return (
-        <div>
-            <Table
-            columns={columns}
-            dataSource={story}
-            pagination={{ position: ["bottomCenter"], pageSize: 8 }}
-            onRow={(record)=>({
-                onClick: ()=> handleDataShow(record)
-            })}
-            >
-            </Table>
-            
+        <div style={{ height: "100vh", width: '100%' }}>
+            <DataGrid
+                rows={story}
+                columns={columns}
+                getRowId={(row :any) => row.objectID}
+                pageSize={12}
+                loading={loading}
+                onSelectionModelChange= {(data)=>{
+                    const select = story.filter((storydt : any) => storydt.objectID ===data[0] );
+                    handleDataShow(select);
+                }}
+
+            />
         </div>
     )
 }
